04291f50e685fbf7192df9fc04edd93823f81a0b
Average Programmer <dotpy9579@gmail.com>
2026-08-11 21:43:59 +0545

    Add files via upload

---
+++ sim.py
@@ -0,0 +1,231 @@
+import math
+import pygame
+import numpy as np
+import pandas as pd
+# import os
+import joblib
+
+class Simulation:
+    def __init__(self, x, y, speed, motor_force, fps,max_speed,mass, altitude, rope_length=2):
+        self.dt=1/fps
+        self.state = {"x": x, "y": y, "speed": speed,
+                      "acceleration": 0, "motor_force": motor_force,
+                    "max_speed": max_speed, "mass": mass, "angle": 0, "angular_velocity": 0,
+            "rope_length": rope_length, "positions": [],
+             "angles": [],
+                    "speeds": [],
+                    "winds": [],
+                    "motforce_array":[],
+                    "acceleration_array":[],
+                    "angular_array":[],
+                    "drag_":[]
+        }
+        self.rho = 1.225*math.exp(-altitude/8500)
+        self.Cd = 1.0
+        self.area =     2
+        self.g=9.81
+        self.altitude = altitude
+        self.wind_speed=0
+
+    def air_resistance(self):
+        v = self.state["speed"]
+        drag=0.5 * self.rho*self.Cd* self.area* v**2
+        return -drag * np.sign(v) 
+
+    def friction(self):
+        coefficient = 30
+        return -coefficient*np.sign(self.state["speed"])
+
+    def wind_force(self):
+        force = (self.wind_speed**2 * 0.05) * np.sign(self.wind_speed)
+        self.state["winds"].append(self.wind_speed)
+        return force
+
+
+    def changeInWind(self, max, min):
+        self.wind_speed += (np.random.uniform(min, max))
+        self.wind_speed = np.clip(self.wind_speed, -80, 80)
+
+    def movement(self):
+        drag = self.air_resistance()
+        friction = self.friction()
+        net_force = self.state["motor_force"] + drag + friction
+        self.state["acceleration"] = net_force / self.state["mass"]
+        self.state["speed"] += self.state["acceleration"] * self.dt
+        self.state["speed"] = np.clip(self.state["speed"], 0, self.state["max_speed"])
+        self.state["x"] += self.state["speed"] * self.dt + 0.5 * self.state["acceleration"] * self.dt**2
+
+    def pendulum(self):
+        s = self.state
+        L = s["rope_length"]
+        wind = self.wind_force()
+        inertia = self.state["mass"] * (L ** 2)
+        angular_acc = (
+            -s["mass"]*self.g * L * math.sin(s["angle"]) 
+            - s["mass"] * s["acceleration"] * L * math.cos(s["angle"]) 
+            + wind * L * math.cos(s["angle"]) 
+            - 1.5 * s["angular_velocity"]
+        ) / inertia
+        s["angular_velocity"] += angular_acc*self.dt
+        s["angular_velocity"] *= 0.98  
+        s["angle"] += s["angular_velocity"] * self.dt
+        s["y"] = math.cos(s["angle"]) * L
+        s["angular_array"].append(s["angular_velocity"])
+        s["drag_"].append(self.air_resistance())
+
+    def step(self):
+        self.movement()
+        self.pendulum()
+        s = self.state
+        s["positions"].append((s["x"], s["y"]))
+        s["angles"].append(s["angle"])
+        s["speeds"].append(s["speed"])
+        s["motforce_array"].append(s["motor_force"])
+        s["acceleration_array"].append(s["acceleration"])
+    # def sensors(self):
+    #     s=self.state
+    #     return {
+    #         "speed": s["speed"],
+    #         "tilt": s["angle"],
+    #         "gyro": s["angular_velocity"],
+    #         "acceleration": s["acceleration"],
+    #         "wind": s["winds"][-1] if s["winds"] else 0.0,
+    #     }
+
+    def clear(self):
+        self.state["x"] = 0
+        self.state["y"] = 0
+        self.state["speed"] = 0
+        self.state["acceleration"] = 0
+        self.state["angle"] = 0
+        self.state["angular_velocity"] = 0
+        self.state["positions"] = []
+        self.state["angles"] = []
+        self.state["speeds"] = []
+        self.state["winds"] = []
+        self.state["motforce_array"] = []
+        self.state["acceleration_array"] = []
+        self.state["angular_array"] = []
+        self.state["drag_"] = []
+
+class DataAnalyze:
+    def __init__(self, sim: Simulation):
+        self.sim = sim
+    def scale(self, var):
+        var=np.array(var)
+        return (var[-1]-np.mean(var))/np.std(var) if np.std(var) != 0 else 0
+
+    def new_features(self):
+        s = self.sim.state
+        s["motforce_array"] = np.array(s["motforce_array"])
+        s["motforce_roc"] = np.concatenate([np.array([0]), np.diff(s["motforce_array"])])
+        s["acceleration_roc"] = np.concatenate([np.array([0]), np.diff(s["acceleration_array"])]) / self.sim.dt
+
+    def save(self, csv):
+        df = pd.DataFrame(self.sim.state)
+        df.to_csv(csv, index=False, header=True, mode='a')
+def main():
+    pygame.init()
+    WIDTH, HEIGHT = 1000, 600
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Gondola & Pendulum Simulation")
+    clock = pygame.time.Clock()
+    FPS = 60
+    sim = Simulation(
+        x=0,
+        y=0,
+        speed=0,
+        motor_force=250,
+        fps=FPS,
+        max_speed=80,
+        mass=50,
+        rope_length=2.5,
+        altitude=1800
+    )
+    font = pygame.font.SysFont("Arial", 18)
+    running = True
+    scale = 40
+    # os.chdir("archive")
+    model= joblib.load("braking_model.pkl")
+    sim2 = DataAnalyze(sim)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    sim.clear()
+                # if event.key == pygame.K_UP:
+                #     sim.wind_speed+=np.random.uniform(20, 30)
+                # if event.key == pygame.K_DOWN:
+                #     sim.wind_speed-=np.random.uniform(20, 30)
+                if event.key == pygame.K_LEFT:
+                    sim.state["motor_force"] -= 10
+                if event.key == pygame.K_RIGHT:
+                    sim.state["motor_force"] += 10
+        sim.step()
+        sim.changeInWind(np.random.uniform(0, 5), np.random.uniform(-5, 0))
+        mass2=sim2.scale(sim.state["mass"])
+        angle2=sim2.scale(sim.state["angles"])
+        angular_velocity2=sim2.scale(sim.state["angular_array"])
+        drag2=sim2.scale(sim.state["drag_"])
+        winds2=sim2.scale(sim.state["winds"])
+        speeds2=sim2.scale(sim.state["speeds"])
+        pred=model.predict([[mass2, angle2, speeds2, winds2, angular_velocity2, drag2]])
+        if pred==1:
+            sim.state["motor_force"] -= 10
+        elif pred==0:
+            sim.state["motor_force"] += 10
+        screen.fill((30, 30, 35))
+        base_screen_x= 100
+        base_screen_y=300
+        world_x_px = sim.state["x"] * scale
+        anchor_x = base_screen_x + (world_x_px % (WIDTH - 200))
+        anchor_y = base_screen_y
+        L_px = sim.state["rope_length"] * scale
+        bob_x = anchor_x + L_px*math.sin(sim.state["angle"])
+        bob_y = anchor_y + L_px*math.cos(sim.state["angle"])
+        pygame.draw.line(screen, (70, 70, 80), (0, anchor_y + 50), (WIDTH, anchor_y + 50), 4)
+        if len(sim.state["positions"]) > 1:
+            points = []
+            for pos in sim.state["positions"][-200:]:
+                px = base_screen_x + (pos[0] * scale) % (WIDTH - 200)
+                py = anchor_y + pos[1] * scale
+                points.append((px, py))
+            if len(points) > 1:
+                pygame.draw.aalines(screen, (70, 130, 180), False, points)
+        pygame.draw.line(screen, (200, 200, 200), (anchor_x, anchor_y), (bob_x, bob_y), 3)
+        pygame.draw.rect(screen, (220, 100, 100), (anchor_x - 20, anchor_y - 15, 40, 30))
+        pygame.draw.circle(screen, (240, 200, 50), (int(bob_x), int(bob_y)), 12)
+        # sensor_data = sim.sensors()
+        # texts = [
+        #             f"Speed: {sensor_data['speed']:.2f} m/s",
+        #             f"Tilt Angle: {math.degrees(sensor_data['tilt']):.2f}°",
+        #             f"Gyro: {sensor_data['gyro']:.2f} rad/s",
+        #             f"Acceleration: {sensor_data['acceleration']:.2f} m/s²",
+        #             f"Wind Force: {sensor_data['wind']:.2f}",
+        #             f"Total Distance X: {sensor_data['x']:.2f} m",
+        #             "[R] Reset Simulation",
+        #         ]
+        
+        texts = [
+            f"Speed: {sim.state['speed']:.2f} m/s",
+            f"Tilt Angle: {math.degrees(sim.state['angle']):.2f}°",
+            f"Gyro: {sim.state['angular_velocity']:.2f} rad/s",
+            f"Acceleration: {sim.state['acceleration']:.2f} m/s²",
+            f"Wind Force: {sim.state['winds'][-1] if sim.state['winds'] else 0.0:.2f}",
+            f"Total Distance X: {sim.state['x']:.2f} m",
+            "[R] Reset Simulation",
+        ]
+        for idx, text in enumerate(texts):
+            render_surface = font.render(text, True, (240, 240, 240))
+            screen.blit(render_surface, (20, 20 + idx * 25))
+        pygame.display.flip()
+        clock.tick(FPS)
+    pygame.quit()
+    # sim2.new_features()
+    # sim2.save("physics.csv")
+
+
+if __name__ == "__main__":
+    main()
---
+++ traineer.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+import joblib #test model saving and loading
+
+#comment these after training, testing and especially before compiling to esp8266
+
+# from sklearn.decomposition import PCA
+#from sklearn.cluster import KMeans
+# import matplotlib.pyplot as plt
+
+#no calibration due to constrains
+
+a = pd.DataFrame(pd.read_csv("physics.csv"))
+a["angular_roc"]=np.concatenate([np.array([0]), np.diff(a['angular_array'])]) 
+a["braking_label"] = (
+    (a["speed"] > 50) &
+    (
+        (abs(a["angular_array"]) > 0.15) |
+        (abs(a["angular_roc"]) > 0.05) |
+        (a["acceleration"] < -2)
+    )
+).astype(int)
+choices = [1]
+y = a['braking_label']
+columns_to_drop = [
+    "positions", "x", "y", "rope_length", "motor_force","max_speed", "acceleration","braking_label", "motforce_array", "acceleration_array", "motforce_roc"
+    ,"acceleration_roc","speed","angular_velocity", "angular_roc","angle"
+]
+a= a.drop(columns=columns_to_drop)
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(a, y, test_size=0.1, random_state=42)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train_raw)
+X_test = scaler.transform(X_test_raw)
+#kmeans = KMeans(n_clusters=4, random_state=42, n_init='auto')
+#cluster_ids = kmeans.fit_predict(scaled_base)
+#cluster_distances = kmeans.transform(scaled_base)
+# engineered_df = pd.DataFrame(scaled_base, columns=base_features_df.columns)
+# engineered_df['cluster_id'] = cluster_ids
+# for i in range(cluster_distances.shape[1]):
+#     engineered_df[f'distance_to_cluster_{i}'] = cluster_distances[:, i]
+# print("Engineered Feature Matrix Sample:")
+# print(engineered_df.head(2))
+# X_vis = PCA(n_components=2).fit_transform(engineered_df)
+# plt.figure(figsize=(11, 7))
+# label_map = {1: 'Instant Brakes (1)', 0: 'Slow Down (0)', -1: 'Pace (-1)'}
+# colors = {1: '#de2d26', 0: '#3182bd', -1: '#31a354'} 
+# for label_val, label_name in label_map.items():
+#     mask = (a['braking_label'].values == label_val)
+#     plt.scatter(
+#         X_vis[mask, 0], 
+#         X_vis[mask, 1], 
+#         label=label_name,
+#         color=colors[label_val],
+#         alpha=0.6,
+#         edgecolors='w',
+#         linewidth=0.5,
+#         s=60
+#     )
+# plt.title('PCA of Enriched Feature Space (Base Features + Cluster Distances)', fontsize=13, fontweight='bold')
+# plt.xlabel('Principal Component 1', fontsize=11)
+# plt.ylabel('Principal Component 2', fontsize=11)
+# plt.grid(True, linestyle='--', alpha=0.5)
+# plt.legend(title="True Threshold Labels", loc='upper right')
+# plt.show()
+
+# model = RandomForestClassifier(n_estimators=15, max_depth=8, random_state=42,
+#                                class_weight="balanced",
+#                                min_samples_leaf=5,
+#                                n_jobs=-1
+#                                )
+# model.fit(X_train, y_train)
+# joblib.dump(model, "braking_model.pkl")
+
+#test
+
+model= joblib.load("braking_model.pkl")
+pred=(model.predict_proba(X_test)>0.5).astype(int)[:,1]
+# pred=model.predict(X_test)
+print(accuracy_score(y_test, pred))
+print(confusion_matrix(y_test, pred))
+print(classification_report(y_test, pred), a)
